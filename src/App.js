efcff4bd83a5acac9e8044d494e720f19ae2e88d
Zandro Diocampo <zandrodiocampo@gmail.com>
2023-05-05 13:22:46 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [showName, setShowName] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleButtonClick = () => {
+    if (name.trim() === '') {
+      setErrorMessage('Please enter your name!');
+      setShowName(false);
+    } else {
+      setShowName(true);
+      setErrorMessage('');
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <label className='label'>
+        What's your name?
+        <br></br>
+        <input type="text" value={name} onChange={handleNameChange} className='inputName'/>
+      </label>
+      <br></br>
+      <button onClick={handleButtonClick} className='button'>Submit</button>
+      {errorMessage && <p className='error'>{errorMessage}</p>}
+      {showName && <p className='output'>Welcome, {name}!</p>}
     </div>
   );
 }
